0e5cd6d0d54bed12ab18928d5bd050e7fe8895cc
Thomas Gjerde <thomasgjerde90@gmail.com>
2016-02-21 12:49:12 +0100

    Possible fix for name prefix problem

--- db/migrate/20160221112719_create_join_table_gangs_positions.rb
+++ db/migrate/20160221112719_create_join_table_gangs_positions.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableGangsPositions < ActiveRecord::Migration[5.0]
   def change
-    create_join_table :gangs, :positions do |t|
+    create_join_table :gangs, :positions, table_name: :alexandrie_gangs_positions do |t|
       # t.index [:gang_id, :position_id]
       # t.index [:position_id, :gang_id]
     end
--- lib/alexandrie/version.rb
+++ lib/alexandrie/version.rb
@@ -1,3 +1,3 @@
 module Alexandrie
-  VERSION = '0.1.4'
+  VERSION = '0.1.4.1'
 end
