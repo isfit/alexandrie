884670faa3d3b06f1ce66f6447a5d0fb061c67ea
Thomas Gjerde <thomasgjerde90@gmail.com>
2016-02-28 11:12:06 +0100

    Fix comma

--- app/models/alexandrie/functionary.rb
+++ app/models/alexandrie/functionary.rb
@@ -3,7 +3,7 @@ module Alexandrie
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and  :registerable, :validatable
     devise :database_authenticatable, :omniauthable,
-           :recoverable, :rememberable, :trackable, 
+           :recoverable, :rememberable, :trackable
   	has_one :postition
   end
 end
