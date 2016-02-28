67a530762ac643a80a363202cf8726b9e12cc4e7
Thomas Gjerde <thomasgjerde90@gmail.com>
2016-02-28 10:43:33 +0100

    Fix spelling error in migration

--- db/migrate/20160226185203_create_alexandrie_functionaries.rb
+++ db/migrate/20160226185203_create_alexandrie_functionaries.rb
@@ -2,7 +2,7 @@ class CreateAlexandrieFunctionaries < ActiveRecord::Migration[5.0]
   def change
     create_table :alexandrie_functionaries do |t|
       t.string :name
-      t.reference :position
+      t.references :position
 
       t.timestamps
     end
