3fd91b0fabaca3b1e04c9aa509bfee732c069e0e
Adrian Alexander Eriksen <adrian@aeriksen.com>
2016-04-05 00:38:42 +0200

    Makes volunteer applicant unregisterable

--- app/models/alexandrie/volunteer_applicant.rb
+++ app/models/alexandrie/volunteer_applicant.rb
@@ -2,8 +2,8 @@ module Alexandrie
   class VolunteerApplicant < ApplicationRecord
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :trackable, :validatable
+    devise :database_authenticatable, :recoverable, :rememberable, :trackable,
+           :validatable
 
     has_one :volunteer_application
     accepts_nested_attributes_for :volunteer_application
